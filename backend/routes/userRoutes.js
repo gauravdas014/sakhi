f97d6638810aa5064e4a946bd4958441273d9aff
imemya19 <45592644+imemya19@users.noreply.github.com>
2020-09-18 15:05:13 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,7 @@ const userController = require("../controllers/userController");
 // router.route("/:email").get(userController.me);
 router.route("/user/:userId").get(userController.read);
 router.route("/user/:userId").put(userController.update);
-router.route("/orders/by/user/:userId").put(userController.purchaseHistory);
+router.route("/orders/by/user/:userId").get(userController.purchaseHistory);
 
 router.param("userId", userController.userById);
 
