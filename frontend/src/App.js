faed6fc2348b8864c2c5f580679beabe1c78bbad
Salil Naik <salil.naik27@gmail.com>
2020-09-18 11:49:06 +0530

    add: header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { IndexPage } from './pages';
+import { IndexPage } from './pages/index.jsx';
 import { AuthPage } from './pages/auth';
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <div>
-
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route path="/" exact render={() => <IndexPage />} />
           <Route path="/auth" exact component={AuthPage} />
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+// import { Menu } from 'antd';
+// import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+
+// const { SubMenu } = Menu;
+
+class Header extends Component {
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({ current: e.key });
+  };
+
+  render() {
+    const { current } = this.state;
+    return (
+      <header className="header">
+        <div className="grid-container">
+          <div className="grid-x grid-margin-x">
+            <div className="cell large-4">
+              Logo
+            </div>
+            <div className="cell large-8">
+              <div className="navbar">
+                <ul className="navbar__item-container">
+                  <NavLink to="/store" className="navbar__item" activeClassName="navbar__item--active">
+                    Store
+                  </NavLink>
+                  <NavLink to="/jobs" className="navbar__item" activeClassName="navbar__item--active">
+                    Jobs
+                  </NavLink>
+                  <NavLink to="/login" className="navbar__item" activeClassName="navbar__item--active">
+                    Login
+                  </NavLink>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
