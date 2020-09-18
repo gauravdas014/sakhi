10820adfe95c879d7bcd4525610d5de6bbc702f8
pkpsyder007 <pkpsyder007@gmail.com>
2020-09-18 11:24:38 +0530

    Added auth in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { IndexPage } from './pages';
 import { AuthPage } from './pages/auth';
