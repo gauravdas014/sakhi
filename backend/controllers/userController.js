f906755304e5c3ad43e4e7ff85f8b39ba338bdbf
pkpsyder007 <pkpsyder007@gmail.com>
2020-09-18 12:59:56 +0530

    Add me in user controoler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -103,3 +103,18 @@ exports.purchaseHistory = async (req, res) => {
       res.json(orders);
     });
 };
+
+exports.me = (req, res) => {
+  User.findOne({ email: req.params.email })
+    .then(user => {
+      if (user) {
+        return res.status(201).json({ auth: true, user });
+      } else {
+        return res.status(503).json({ auth: false, user });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(501).json({ auth: false, user: null });
+    })
+} 
